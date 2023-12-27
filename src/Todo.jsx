5642ba6e2365e1c42c2a159a55b2a6deb08ffa9f
hiro-mu <wad.akico.0926@icloud.com>
2023-12-27 17:01:37 +0900

    制限の追加

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -40,13 +40,21 @@ export const Todo = () => {
     setCompleteTodos(newCompleteTodos);
   };
 
+  const isMaxLimitIncompleteTodos = incompleteTodos.length >= 5;
+
   return (
     <>
       <InputTodo
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={isMaxLimitIncompleteTodos}
       />
+      {isMaxLimitIncompleteTodos && (
+        <p style={{ color: "red" }}>
+          登録できるTODOは5個までだよ〜。消化しろ~。
+        </p>
+      )}
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,12 +1,24 @@
-export const InputTodo = ({ todoText, onChange, onClick }) => {
+const style = {
+  backgroundColor: "#c6e5d9",
+  height: "36px",
+  width: "400px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "8px",
+};
+
+export const InputTodo = ({ todoText, onChange, onClick, disabled }) => {
   return (
-    <div className="input-area">
+    <div style={style}>
       <input
+        disabled={disabled}
         placeholder="TODOを入力"
         value={todoText}
         onChange={onChange}
       />
-      <button onClick={onClick}>追加</button>
+      <button disabled={disabled} onClick={onClick}>
+        追加
+      </button>
     </div>
   );
 };
